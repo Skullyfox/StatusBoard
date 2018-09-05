8ac5eccb5296011b4f278c4f18b235f567ef87a5
Skullyfox <skullyfox.yt@gmail.com>
2018-09-05 10:20:01 +0200

    add content cards

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,9 @@ const Cards = document.querySelectorAll('.Card');
 
 Cards.forEach( elem => {
   elem.addEventListener('click', () => {
-    elem.classList.toggle('on')
+    elem.classList.toggle('on');
+    elem.classList.contains('on') 
+      ? elem.querySelector('.CardStatus').innerText = "On" 
+      : elem.querySelector('.CardStatus').innerText = "Off";
   })
 });
